7851baf403bf3d70447bf6c06a74301e0925e547
anhba817 <dong.ng.tan@gmail.com>
2021-07-28 23:36:07 +0700

    Common components that can be reused

---
+++ src/app/core/components/StatisticCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const StatisticCard = ({ title, value, total, showPercentage = false }) => {
+  return (
+    <div className="text-center">
+      <div>{title}</div>
+      <div className="font-size-24">{value}</div>
+      {showPercentage && <div>{Math.round((value * 10000) / total) / 100.0}%</div>}
+    </div>
+  );
+};
+
+export default StatisticCard;
